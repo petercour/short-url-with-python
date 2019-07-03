d4162e5e846b0cdded6ded893c36dae7ce60c217
Dev <frank@fastmail.org>
2019-07-03 15:44:21 +0200

    Short URL in Python

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Tiny URL with Python
+Function to create short URLS using tinyurl service.
+
+https://pythonbasics.org
+https://pythonprogramminglanguage.com
---
+++ turl.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+
+def tiny_url(url):
+    apiurl = "http://tinyurl.com/api-create.php?url="
+    tinyurl = urllib.request.urlopen(apiurl + url).read()
+    return tinyurl.decode("utf-8") 
+
+url = sys.argv[1]
+print(url)
+print(tiny_url(url))
